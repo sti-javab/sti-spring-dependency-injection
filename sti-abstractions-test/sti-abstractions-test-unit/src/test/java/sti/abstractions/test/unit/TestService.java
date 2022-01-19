121d24aedd53fe6d133bf669328668e116a59162
zgrkbr <ozgurkibar78@gmail.com>
2022-01-19 11:43:45 +0100

    add TestService

---
+++ sti-abstractions-test/sti-abstractions-test-unit/src/test/java/sti/abstractions/test/unit/TestService.java
@@ -0,0 +1,32 @@
+package sti.abstractions.test.unit;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import sti.abstractions.domain.Owl;
+import sti.abstractions.service.ForestService;
+
+public class TestService {
+
+    private final ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:sti-abstractions-service.xml");
+
+    private ForestService forestService;
+
+    private ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    @Before
+    public void before() {
+        forestService = (ForestService) getApplicationContext().getBean("forestService");
+    }
+
+    @Test
+    public void testDependencyInjection(){
+        Owl owl = forestService.createOwl(90);
+        Assert.assertNotNull(owl);
+    }
+
+}
